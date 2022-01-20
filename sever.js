c79b85e5a17181ac00d143a35b514bcf28f7a65b
ranamalsingh12 <ranams99911@gmail.com>
2022-01-20 11:19:38 +0530

    connecting to mongo db

--- sever.js
+++ sever.js
@@ -4,12 +4,17 @@ const morgan = require("morgan")
 const bodyparser = require("body-parser")
 const path = require("path")
 const app = express();
+const connectDB = require('./server/database/connection')
 
 dotenv.config({path : 'config.env'})
 const PORT = process.env.PORT || 8080
 
 //log request 
 app.use(morgan('tiny'))
+
+//MongoDB connection
+connectDB();
+
 
 //pass request to body parser
 app.use(bodyparser.urlencoded({extended :true}))
@@ -18,7 +23,6 @@ app.use(bodyparser.urlencoded({extended :true}))
 app.set("view engine","ejs")
 
 // load assets
-
 app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
 app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
 app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
