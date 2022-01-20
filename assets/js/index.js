b2498adc4ecdd0735416f982264d5a6df6b02926
ranamalsingh12 <ranams99911@gmail.com>
2022-01-20 16:42:30 +0530

    final

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,4 @@
-// const update = document.getElementById('update-user');
-// const app = document.getElementById('app-user');
-
-// update.onsubmit(function (event) {
-//     event.preventDefault();
-//     alert("Data inserted successfully")
-// })
-
+console.log("hello world");
 // ${"#update-user"}.submit(function (event) {
 //     alert("Data Inserted Successfully");
 // })
@@ -13,3 +6,21 @@
 // ${"#app-user"}.submit(function (event) {
 //     alert("Data Inserted Successfully");
 // })
+
+// if(window.location.path == '/'){
+//     $ondelete = $('.table tbody tr td a.delete');
+//     $ondelete.click(function(){
+//         console.log("hello");
+//         var id = $(this).attr("data-id")
+
+//         var request = {
+//             "url" : `http://localhost:3000/api/users/${id}`,
+//             "method" : "PUT"
+//         }
+//         if(confirm("Do you really want to delete this record")){
+//             $.ajax(request).done(function(response){
+//                 alert("Data Deleted Successfully")
+//             })
+//         }
+//     })
+// }
--- sever.js
+++ sever.js
@@ -26,6 +26,7 @@ app.set("view engine","ejs")
 app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
 app.use('/img',express.static(path.resolve(__dirname,"assets/img")))
 app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
+// app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
 
 //Load router
 app.use('/',require('./server/routes/router'))
