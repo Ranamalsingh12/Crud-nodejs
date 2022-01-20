1bfc502a8995c32a8b7f340bdf51859e1590cba1
ranamalsingh12 <ranams99911@gmail.com>
2022-01-20 12:15:20 +0530

    updating the value of user by id

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -31,12 +31,39 @@ exports.create = (req, res) => {
 
 //retrive and return all user or same user
 exports.find = (req, res) => {
-
+    Userdb.find()
+    .then(user => {
+        res.send(user)
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occured while creating a operation"
+        });
+    });
+    
 }
 
 //Update a new identifier user by user id
 exports.update = (req, res) => {
-
+    if(!req.body){
+        return res.status(400)
+        .send({message : "data to update cannot be empty"})
+    }
+    
+    const id = req.params.id
+    Userdb.findByIdAndUpdate(id,req.body,{ useFindAndModify : false})
+    .then(data => {
+        if(!data){
+            res.status(404).send({message : `Cannot Update users with ${id}. User not found`})
+        }else{
+            res.send(data)
+        }
+    })
+    .catch(err => {
+        res.status(500).send({
+            message: err.message || "Error update user information"
+        });
+    });
 }
 
 //Delete a user with specified user id
