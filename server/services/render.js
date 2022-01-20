04bfe267b02c55122ec5c003f776a7330b48864b
ranamalsingh12 <ranams99911@gmail.com>
2022-01-20 12:53:31 +0530

    showing the database value on screen

--- server/services/render.js
+++ server/services/render.js
@@ -1,5 +1,16 @@
+const axios = require('axios');
+
 exports.homeRoutes = (req, res) => {
-    res.render('index',{ users : "New Data"});
+    //Make a get request to /api/users
+    axios.get('http://localhost:3000/api/users')
+    .then(function (response) {
+        res.render('index',{ users : response.data});
+    })
+    .catch(err => {
+        res.send(err);
+    })
+
+
 }
 
 exports.new_user = (req, res) => {
